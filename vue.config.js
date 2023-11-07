361e1fa68f5d68c0b0cd086d806d960cc09c414a
SamThicker <704851103@qq.com>
2023-11-07 16:44:46 +0800

    反向代理静态资源

--- vue.config.js
+++ vue.config.js
@@ -29,6 +29,11 @@ module.exports = {
         pathRewrite: {
           "^/raw": ""
         }
+      },
+      "/static": {
+        target: "http://127.0.0.1:9000", //对应自己的接口
+        changeOrigin: true,
+        ws: true
       }
     }
   }
